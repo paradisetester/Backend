4c3933c6d355c23c7b47246aef4c1493f385a766
Dhruv Gupta <niteshkumar@paradisetechsoft.com>
2025-04-28 16:02:15 +0530

    updated the porfolio routes

--- routes/PortfolioRoutes/projectsRoutes.js
+++ routes/PortfolioRoutes/projectsRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Project = require('../models/Porfolio/Projects');
+const Project = require('../models/Portfolio/Projects');
 // const authenticate = require('../middleware/authenticate'); // uncomment if you have auth
 
 const router = express.Router();
